3a123dd94824c8c2e23bfc1b3755d0c903b59658
anthony besson <besson.antho@hotmail.fr>
2016-11-08 16:31:48 +0100

    add bailleurs form

--- app/models/bailleur.rb
+++ app/models/bailleur.rb
@@ -1,7 +1,7 @@
 class Bailleur < ApplicationRecord
   belongs_to :user
 
-  CATEGORIES = ["Mr", "Mme", "Mr et Mme", "Mr et Mr", "Mme et Mme", "Sci", "Sci familiale"]
+  CIVILITES = ["Mr", "Mme", "Mr et Mme", "Mr et Mr", "Mme et Mme", "Sci", "Sci familiale"]
   validates :name, presence: true
-  validates :civilite, inclusion: { in: CATEGORIES }
+  validates :civilite, inclusion: { in: CIVILITES }
 end
