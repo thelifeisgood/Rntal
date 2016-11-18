458f396ae7f85a4a5132cb35bd6b71654ae5ce20
anthony besson <besson.antho@hotmail.fr>
2016-11-18 07:32:38 +0100

    daily save

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,9 @@
 class DashboardsController < ApplicationController
-  def search
+  before_action :set_dashboard
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_dashboard
     @bailleurs = current_user.bailleurs(params[:id])
-  end
+    end
 end
